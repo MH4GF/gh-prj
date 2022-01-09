b5a8b0e2bf75cd0072532f704d5647be22c5c169
MH4GF <h.miyagi.cnw@gmail.com>
2022-01-09 17:11:05 +0900

    change runs on macos

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
-cargo build --release --target $TARGET
-mv "target/${TARGET}/release/gh-prj" "./dist/${FILENAME}"
+platforms="$(cat << EOL
+  [
+    { "target": "x86_64-apple-darwin", "name": "darwin-amd64" },
+    { "target": "aarch64-apple-darwin", "name": "darwin-arm64" },
+    { "target": "x86_64-unknown-linux-musl", "name": "linux-amd64" }
+  ]
+EOL
+)"
+platforms_end_index="$(echo "${platforms}" | jq '. | length - 1')"
+
+for i in $(seq 0 "${platforms_end_index}"); do
+    target=$(echo "${platforms}" | jq -r ".[${i}].target")
+    name=$(echo "${platforms}" | jq -r ".[${i}].name")
+
+    cargo build --release --target $target
+    mv "target/${target}/release/gh-prj" "./dist/${name}"
+done
