ba69d70e386694d82d24808f48e51e8bfef8e086
MH4GF <h.miyagi.cnw@gmail.com>
2022-01-14 23:34:48 +0900

    add view command test

--- tests/cmd_test.rs
+++ tests/cmd_test.rs
@@ -1,6 +1,13 @@
 extern crate gh_prj;
 
+/// fetch data from https://github.com/MH4GF/gh-prj/projects
 #[test]
 fn it_works_list_cmd() {
     assert!(gh_prj::cmd::list::list_prj());
+}
+
+/// fetch data from https://github.com/MH4GF/gh-prj/projects/1
+#[test]
+fn it_works_view_cmd() {
+    assert!(gh_prj::cmd::view::view_prj(false, 1));
 }
